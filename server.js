31b079b8fe436ec4ae19646cb24063d0b1b584a5
Steve <steve@yarrumevets.com>
2025-07-20 23:07:27 -0700

    Fixed path issue with dotenv.

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Load shared .env
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+// Load app-specific .env if present (if/when added)
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 dotenv.config({ path: "../.env" });
 dotenv.config();
 
